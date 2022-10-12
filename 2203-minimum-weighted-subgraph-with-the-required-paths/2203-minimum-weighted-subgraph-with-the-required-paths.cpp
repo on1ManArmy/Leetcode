67058b5ebc3314f3a7a1680cc87114da2a567483
Abhishek Kumar <iitjeeabhishek@gmail.com>
2022-10-12 18:39:19 +0530

    Time: 1367 ms (23.93%), Space: 144.3 MB (53.66%) - LeetHub

--- 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -1,57 +1,56 @@
 typedef long long ll;
 class Solution {
 public:
-//     vector<long long> dijkstara(vector<vector<pair<int, long long>>>& g, int src){
-//         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-//         int n = g.size();
-//         vector<int> cost(n);
-        
-//         cost[src] = 0;
-//         pq.push(make_pair(0, src));
-        
-//         while(!pq.empty()){
-            
-//             int cost = pq.top().first;
-//             int node = pq.top().second;
-//             pq.pop();
-            
-//             // vector<pair<long long, int>>:: iterator it;
-//             for(auto it: g[node]){
-//                 int nextNode = it.first;
-//                 int costNext = it.second;
-                
-//                 if(cost[nextNode] > cost[node] + costNext){
-//                     cost[nextNode] = cost[node] + costNext;
-//                     pq.push(make_pair(cost[node], nextNode));
-//                 }
-//             }
-            
-//         }
-        
-//         return cost;
-        
-//     }
-    
     const ll kInf = 1e18;
-	vector<ll> dijkstra(vector<vector<pair<int, ll>>>& adj, int src) {
-		int n = adj.size();
-		vector<ll> dist(n, kInf);
-		dist[src] = 0;
-		priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
-		// pq contains pairs of <the current distance estimate from src to id, id>
-		pq.push(make_pair(0, src));
-		while (!pq.empty()) {
-			auto [d, cur] = pq.top(); pq.pop();
-			if (d != dist[cur]) continue;
-			for (auto [nei, w] : adj[cur]) {
-				if (dist[nei] > d + w) {
-					dist[nei] = d + w;
-					pq.push(make_pair(dist[nei], nei));
-				}
-			}
-		}
-		return dist;
-	}
+    vector<long long> dijkstra(vector<vector<pair<int, long long>>>& g, int src){
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        int n = g.size();
+        vector<ll> cost(n, kInf);
+        
+        cost[src] = 0;
+        pq.push(make_pair(0, src));
+        
+        while(!pq.empty()){
+            
+            auto [fromCost, fromNode] = pq.top();
+            pq.pop();
+            
+            if (fromCost != cost[fromNode]) continue;
+            
+            // vector<pair<long long, int>>:: iterator it;
+            for(auto [toNode, toCost]: g[fromNode]){
+                if(cost[toNode] > fromCost + toCost){
+                    cost[toNode] = fromCost + toCost;
+                    pq.push(make_pair(cost[toNode], toNode));
+                }
+            }
+            
+        }
+        
+        return cost;
+        
+    }
+    
+    
+// 	vector<ll> dijkstra(vector<vector<pair<int, ll>>>& adj, int src) {
+// 		int n = adj.size();
+// 		vector<ll> dist(n, kInf);
+// 		dist[src] = 0;
+// 		priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+// 		// pq contains pairs of <the current distance estimate from src to id, id>
+// 		pq.push(make_pair(0, src));
+// 		while (!pq.empty()) {
+// 			auto [d, cur] = pq.top(); pq.pop();
+// 			if (d != dist[cur]) continue;
+// 			for (auto [nei, w] : adj[cur]) {
+// 				if (dist[nei] > d + w) {
+// 					dist[nei] = d + w;
+// 					pq.push(make_pair(dist[nei], nei));
+// 				}
+// 			}
+// 		}
+// 		return dist;
+// 	}
     
     long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
        vector<vector<pair<int, ll>>> adj(n);
