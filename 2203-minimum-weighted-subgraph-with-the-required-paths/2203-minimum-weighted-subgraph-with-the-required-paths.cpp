9c17c5a48a4e2a8183e26259f2d1293d59df1c0f
Abhishek Kumar <iitjeeabhishek@gmail.com>
2022-10-12 18:42:06 +0530

    Time: 1294 ms (33.63%), Space: 144.2 MB (53.66%) - LeetHub

--- 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -53,22 +53,24 @@ public:
 // 	}
     
     long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
-       vector<vector<pair<int, ll>>> adj(n);
+        vector<vector<pair<int, ll>>> adj(n);
 		vector<vector<pair<int, ll>>> rev(n);  // the graph with reversed edges
-		int u, v, w;
+
 		for (auto& e : edges) {
-			u = e[0], v = e[1], w = e[2];
+			int u = e[0];
+            int v = e[1];
+            int w = e[2];
 			adj[u].push_back(make_pair(v, w));
 			rev[v].push_back(make_pair(u, w));
 		}
 		// Dijkstra
-		vector<ll> dist_from_src1 = dijkstra(adj, src1);
-		vector<ll> dist_from_src2 = dijkstra(adj, src2);
-		vector<ll> dist_to_dest   = dijkstra(rev, dest);
+		vector<ll> subGraph1 = dijkstra(adj, src1);
+		vector<ll> subGraph2 = dijkstra(adj, src2);
+		vector<ll> subGraph3   = dijkstra(rev, dest);
 		// find the smallest w[i]
 		ll ans = kInf;
 		for (int i = 0; i < n; i++) {
-			ans = min(ans, dist_from_src1[i] + dist_from_src2[i] + dist_to_dest[i]);
+			ans = min(ans, subGraph1[i] + subGraph2[i] + subGraph3[i]);
 		}
 		return ans == kInf ? -1 : ans;
     }
